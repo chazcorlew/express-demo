417aacebe67ff22d1959428047bd4cf62bc3c8b3
Chaz Corlew <chaz.corlew@gmail.com>
2022-12-06 12:46:05 -0800

    created express JS demo

---
+++ routes/users.js
@@ -0,0 +1,53 @@
+
+const express = require('express')
+const router = express.Router()
+
+
+
+router.get('/', (req, res) => {
+    console.log(req.query.name)
+    res.send("User List")
+
+
+})
+
+router.get('/new', (req, res) => {
+    res.render('users/new')
+
+})
+
+router.post('/', (req, res) => {
+    const isValid = false
+    if( isValid) {
+        users.push({firstName: req.body.firstName})
+        res.redirect(`/users/${users.length - 1}`)
+    } else {
+        
+        res.render("users/new", { firstName: req.body.firstName})
+    }
+
+})
+
+router
+.route('/:id')
+.get((req, res) => {
+    console.log(req.user)
+    res.send(`Get User With ID ${req.params.id}`)
+})
+.put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`)
+})
+
+
+const users = [{ name: "Kevin" }, {name: "James" }]
+router.param('id', (req, res, next, id) => {
+req.user = users[id]
+next()
+})
+ 
+
+
+module.exports = router
